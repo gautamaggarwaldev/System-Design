d696d48d920648556f9346239caaef8c717f14c3
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-10-27 13:24:28 +0530

    strategy design pattern

---
+++ 7. Design Patterns in LLD/1. Strategy Design Pattern/StrategyDesignPatternWithoutInheritance.cpp
@@ -0,0 +1,143 @@
+# include <iostream>
+using namespace std;
+
+class Walkable {
+public:
+    virtual void walk() = 0;
+    virtual ~Walkable() {};
+};
+
+class NormalWalk : public Walkable {
+public:
+    void walk() override {
+        cout << "Normal walk initialised..." << endl;
+    }
+};
+
+class NoWalk : public Walkable {
+public:
+    void walk() override {
+        cout << "No walk functionality implemented..." << endl;
+    }
+};
+
+class Talkable {
+public:
+    virtual void talk() = 0;
+    virtual ~Talkable() {};
+};
+
+class NormalTalk : public Talkable {
+public:
+    void talk() override {
+        cout << "Normal talk initialised..." << endl;
+    }
+};
+
+class NoTalk : public Talkable {
+public:
+    void talk() override {
+        cout << "No talk functionality implemented..." << endl;
+    }
+};
+
+class Flyable {
+public:
+    virtual void fly() = 0;
+    virtual ~Flyable() {};
+};
+
+class NormalFly : public Flyable {
+public:
+    void fly() override {
+        cout << "Normal fly initialised..." << endl;
+    }
+};
+
+class NoFly : public Flyable {
+public:
+    void fly() override {
+        cout << "No fly functionality implemented..." << endl;
+    }
+};
+
+class Projectionable {
+public:
+    virtual void projection() = 0;
+    virtual ~Projectionable() {};
+};
+
+class CompanionProjection : public Projectionable {
+public:
+    void projection() override {
+        cout << "companion projection initialised..." << endl;
+    }
+};
+
+class WrokerProjection : public Projectionable {
+public:
+    void projection() override {
+        cout << "worker projection functionality implemented..." << endl;
+    }
+};
+
+
+class Robot {
+protected:
+    Walkable* walkbehaviour;
+    Talkable* talkbehaviour;
+    Flyable* flybehaviour;
+    Projectionable* projectionbehaviour;
+public:
+    Robot(Walkable* w, Talkable* t, Flyable* f, Projectionable* p) {
+        this->walkbehaviour = w;
+        this->talkbehaviour = t;
+        this->flybehaviour = f;
+        this->projectionbehaviour = p;
+    }
+
+    void walk() {
+        walkbehaviour->walk();
+    }
+
+    void talk() {
+        talkbehaviour->talk();
+    }
+
+    void fly() {
+        flybehaviour->fly();
+    }
+
+    void projection() {
+        projectionbehaviour->projection();
+    }
+};
+
+class CompanionRobot : public Robot {
+public:
+    CompanionRobot(Walkable* w, Talkable* t, Flyable* f, Projectionable* p) : Robot(w,t,f,p) {}
+    
+};
+
+class WorkerRobot : public Robot {
+public:
+    WorkerRobot(Walkable* w, Talkable* t, Flyable* f, Projectionable* p) : Robot(w,t,f,p) {}
+};
+
+int main() {
+
+    Robot* r1 = new CompanionRobot(new NormalWalk(), new NormalTalk(), new NoFly(), new CompanionProjection());
+    r1->walk();
+    r1->talk();
+    r1->fly();
+    r1->projection();
+
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+    Robot* r2 = new WorkerRobot(new NoWalk(), new NormalTalk(), new NormalFly(), new WrokerProjection());
+    r2->walk();
+    r2->talk();
+    r2->fly();
+    r2->projection();
+   
+}
